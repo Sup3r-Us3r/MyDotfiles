43178ae4d45870ce03a1df5746bfdb8a04208d77
Sup3r-Us3r <pc.gam3rs.tuto@gmail.com>
2018-10-13 17:04:32 -0300

    dots

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+source "${HOME}/.cache/wal/colors.sh"
+background=$color0
+background_alt=$color3
+foreground=$color15
+foreground_alt=$color2
+highlight=$color4
+
+polybar -r arch &

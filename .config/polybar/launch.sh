36ed57b222c0a02d6eb9902a08244c3b87ac3906
Magno Tutor <pc.gam3rs.tuto@gmail.com>
2019-12-23 12:38:13 -0300

    dots

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,13 +4,14 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar > /dev/null; do sleep 1; done
 
-source "${HOME}/.cache/wal/colors.sh"
-background=$color0
-background_alt=$color3
-foreground=$color15
-foreground_alt=$color2
-highlight=$color4
+# Wal Config
+#source "${HOME}/.cache/wal/colors.sh"
+#background=$color0
+#background_alt=$color3
+#foreground=$color15
+#foreground_alt=$color2
+#highlight=$color4
 
 polybar -r arch &

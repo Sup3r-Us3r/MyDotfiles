b8d6ebd432b2ab93dbb46d64c0874a1077e5d404
Sup3r-Us3r <pc.gam3rs.tuto@gmail.com>
2019-12-26 16:34:54 -0300

    dots

--- dependencies.sh
+++ dependencies.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # ALL DEPENDENCIES
-#sudo pacman -S i3 i3lock lm_sensors hddtemp playerctl leafpad termite compton nautilus feh mpc alsa-lib alsa-utils mpd ncmpcpp conky scrot nitrogen rofi lxappearance wireless_tools zsh git ttf-dejavu
+#sudo pacman -S i3 i3lock lm_sensors hddtemp playerctl leafpad termite picom compton nautilus feh mpc alsa-lib alsa-utils mpd ncmpcpp conky scrot nitrogen rofi lxappearance wireless_tools zsh git ttf-dejavu
 #yay -S polybar i3blocks dmenu2 lemonbar-git dmenu2 lemonbar-git i3-gaps ttf-inconsolata ttf-font-awesome ttf-fantasque-sans-mono siji-git
 
 # REQUIRED DEPENDENCIES
 echo "INSTALLING DEPENDENCE NEEDED FOR SETTINGS\n"
-sudo pacman -S i3 i3lock lm_sensors playerctl leafpad termite compton nautilus feh alsa-lib alsa-utils scrot rofi lxappearance wireless_tools zsh git ttf-dejavu
+sudo pacman -S i3 i3lock lm_sensors playerctl leafpad termite compton picom nautilus feh alsa-lib alsa-utils scrot rofi lxappearance wireless_tools zsh git ttf-dejavu
 yay -S polybar i3-gaps ttf-inconsolata ttf-fantasque-sans-mono siji-git
 
 clear

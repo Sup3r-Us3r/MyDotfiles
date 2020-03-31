49a079767f59a6c992c1b80b9a4588b1931c6049
Magno Tutor <maydersonmello@gmail.com>
2020-03-31 19:14:03 -0300

    dots

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -14,4 +14,12 @@ while pgrep -x polybar > /dev/null; do sleep 1; done
 #foreground_alt=$color2
 #highlight=$color4
 
-polybar -r arch &
+#polybar -r arch &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar -r arch &
+  done
+else
+  polybar -r arch &
+fi
